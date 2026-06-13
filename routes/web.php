8d8485eb90af78bf9fdb2e53ee00c1591b0ae3cf
raydanielg <airezra2+1@gmail.com>
2026-06-13 17:47:13 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ Route::get('/', function () {
     return view('landing');
 })->name('home');
 
+// Products page
+Route::get('/products', function () {
+    return view('products');
+})->name('products');
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('user.home')->middleware('auth');

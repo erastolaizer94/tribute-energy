8872f81b9f44b6aecb514f4decac4cf2df40ec84
raydanielg <airezra2+1@gmail.com>
2026-06-13 17:19:10 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// Public pages
+Route::get('/',         [PagesController::class, 'home'])->name('home');
+Route::get('/features', [PagesController::class, 'features'])->name('features');
+Route::get('/products', [PagesController::class, 'products'])->name('products');
+Route::get('/pricing',  [PagesController::class, 'pricing'])->name('pricing');
+Route::get('/about',    [PagesController::class, 'about'])->name('about');
+Route::get('/terms',    [PagesController::class, 'terms'])->name('terms');
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('user.dashboard');
 });

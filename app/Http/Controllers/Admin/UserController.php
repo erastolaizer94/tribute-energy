2ba40960b761ee8cab1e2546c1ededf9677d6fd8
raydanielg <airezra2+1@gmail.com>
2026-06-13 19:28:31 +0300

    commit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $users = User::all();
+        $users = User::with('roles')->get();
         return view('admin.users.index', compact('users'));
     }
 

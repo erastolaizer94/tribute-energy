0affe2650da8ff45c69d7b16e45cc8b2758fb14a
raydanielg <airezra2+1@gmail.com>
2026-06-13 19:19:48 +0300

    commit

--- app/Models/ActivityHistory.php
+++ app/Models/ActivityHistory.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class ActivityHistory extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'activity_type',
+        'description',
+        'related_type',
+        'related_id',
+        'metadata',
+        'ip_address',
+        'user_agent',
+    ];
+
+    protected $casts = [
+        'metadata' => 'array',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'email',
+        'phone',
+        'subject',
+        'message',
+        'status',
+        'replied_by',
+        'reply',
+        'replied_at',
+    ];
+
+    protected $casts = [
+        'replied_at' => 'datetime',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function repliedBy()
+    {
+        return $this->belongsTo(User::class, 'replied_by');
+    }
 }

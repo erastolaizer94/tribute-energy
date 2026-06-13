c836eae7653563fb8c1aca0b54c16d3eff5a2e8a
raydanielg <airezra2+1@gmail.com>
2026-06-13 23:10:04 +0300

    commit

--- database/migrations/2026_06_13_200940_add_frontend_columns_to_products.php
+++ database/migrations/2026_06_13_200940_add_frontend_columns_to_products.php
@@ -14,7 +14,11 @@ class AddFrontendColumnsToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('slug')->nullable()->after('name');
+            $table->string('color_start')->nullable()->after('color');
+            $table->string('color_end')->nullable()->after('color_start');
+            $table->string('flavor')->nullable()->after('color_end');
+            $table->string('size')->nullable()->after('flavor');
         });
     }
 
@@ -26,7 +30,7 @@ class AddFrontendColumnsToProducts extends Migration
     public function down()
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn(['slug', 'color_start', 'color_end', 'flavor', 'size']);
         });
     }
 }

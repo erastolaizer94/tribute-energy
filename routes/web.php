35180869793b3379fe821b252be02f8154d0121c
raydanielg <airezra2+1@gmail.com>
2026-06-13 17:20:31 +0300

    commit

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('user.dashboard');
+    Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
 });
